78746807452f07bdbc1396464e0a63ff9b0b5fd9
Kacper Pham Quoc <kacper.pham_quoc.ext@nokia.com>
2016-11-17 11:53:33 +0100

    bezsensowna zmiana exercise 3

--- vector2.cpp
+++ vector2.cpp
@@ -5,17 +5,19 @@
 int main()
 {
 	std::vector<int> myVector;
-	std::cout << "Size: " << myVector.size() << std::endl;
-	std::cout << "Capacity: " << myVector.capacity() << std::endl;
+		std::cout << "Size: " << myVector.size() << std::endl;
+		std::cout << "Capacity: " << myVector.capacity() << std::endl;
+	
 	myVector.resize(10, 5);
-	std::cout << "Size resize: " << myVector.size() << std::endl;
-	std::cout << "Capacity resize: " << myVector.capacity() << std::endl;
+		std::cout << "Size resize: " << myVector.size() << std::endl;
+		std::cout << "Capacity resize: " << myVector.capacity() << std::endl;
+	
 	myVector.reserve(20);
-	std::cout << "Size reserve: " << myVector.size() << std::endl;
-	std::cout << "Capacity reserve: " << myVector.capacity() << std::endl;
+		std::cout << "Size reserve: " << myVector.size() << std::endl;
+		std::cout << "Capacity reserve: " << myVector.capacity() << std::endl;
+	
 	myVector.shrink_to_fit();
-	std::cout << "Size shrink: " << myVector.size() << std::endl;
-	std::cout << "Capacity shrink: " << myVector.capacity() << std::endl;
-
+		std::cout << "Size shrink: " << myVector.size() << std::endl;
+		std::cout << "Capacity shrink: " << myVector.capacity() << std::endl;
 }
 
