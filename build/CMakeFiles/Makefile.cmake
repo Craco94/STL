d39e250dff715501640f8fd43f669dbf6589a116
Kacper Pham Quoc <kacper.pham_quoc.ext@nokia.com>
2016-11-17 11:48:20 +0100

    after exercise3 (resize, reserve, shrink_to_fit)

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -41,6 +41,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/vector_exe.dir/DependInfo.cmake"
   "CMakeFiles/array_exe.dir/DependInfo.cmake"
+  "CMakeFiles/vector_exe.dir/DependInfo.cmake"
+  "CMakeFiles/vector2_exe.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/vector2_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector2_exe.dir/vector2.cpp.o"
+  "vector2_exe.pdb"
+  "vector2_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vector2_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
